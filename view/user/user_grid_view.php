1c7ca51f29238a62a7bdb3bd3cb0d7718987132d
farah-breik <Farah Breik@172.19.100.35>
2015-04-21 10:51:57 +0300

    file

--- view/user/user_grid_view.php
+++ view/user/user_grid_view.php
@@ -8,8 +8,11 @@ include_once'/modle/DBconnection.php';
     }
 
 function generate_grid(){
-    echo $view="<a href='./index.php?url=user_controler/create/' value=\"create\" \">create</a>";
-
+    echo'<div class="create">';
+    echo $view="<a href='./index.php?url=user_controler/create/' value=\"create\" \">"
+            . "<img src='theams/img/create-button.jpg' height='50px' width='110px'></a>";
+     echo'</div>';
+    
   $connect=  new createConnection ();
   $connect->connectToDatabase();
  $s="SELECT * FROM user_profile  ";
@@ -34,22 +37,27 @@ $sql=mysqli_query($connect->conn,$s);
 $dd = new OPCDataGrid();
 $dd->source($mydata);
 $dd->fields(array(
-' user_id'=>' user id'."<span>",
+' user_id'=>' user id',
  'user_name' => 'user name ',
  'f_nmae'=>'first name',
     'delete'=>'delete',
     'update'=>'update',
     'view'=>'view',
 ));
-$dd->render1();
+//echo '<div class="gird-div">';
+echo $dd->render1();
 
  
 
 
 while($row=  mysqli_fetch_array($sql)){
- $delete="<a href='./index.php?url=user_controler/delete/".$row['user_id']."' value=\"delete\" \">delete</a>";
-  $update="<a href='./index.php?url=user_controler/update/".$row['user_id']."' value=\"update\" \">update</a>";
-   $view="<a href='./index.php?url=user_controler/view/".$row['user_id']."' value=\"view\" \">view</a>";
+ $delete="<a href='./index.php?url=user_controler/delete/".$row['user_id']."' value=\"delete\" \">"
+         . "<img src='theams/img/d.png' height='20px' width='30px'></a>";
+  $update="<a href='./index.php?url=user_controler/update/".$row['user_id']."' value=\"update\" \">"
+          . "<img src='theams/img/edit-icon.gif' height='20px' width='30px'></a>";
+   $view="<a href='./index.php?url=user_controler/view/".$row['user_id']."' value=\"view\" \">"
+           . "<img src='theams/img/v.png' height='20px' width='30px'></a>"; 
+
 $data = array(
  array(
  'uid' =>$row['user_id'],
@@ -62,7 +70,6 @@ $data = array(
      'view'=>$view,
  ),
  ); 
- 
  
 
 
@@ -77,10 +84,12 @@ $dg->fields(array(
     'view'=>'',
  ));
 
- $dg->render2();
+ echo $dg->render2();
 
+//echo '</div>';
 
 }
+echo '</table>';
 }
 }
 ?>
