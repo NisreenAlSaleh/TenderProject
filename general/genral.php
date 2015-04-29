dfa2d6f244f9b7ac8b49f6f5e33d546d134b03b2
saqerhasan <saqer.m.hasan@gmail.com>
2015-04-29 11:03:55 +0300

    gethub commit test

--- general/genral.php
+++ general/genral.php
@@ -1,14 +1,39 @@
 <?php
+include_once 'autharization.php';
 class Genral{
    
 function generateMenu($items) {
-  
+ //   echo'this the genarate method';
+    
+    //print_r($items);
+    
+ 
   $html = "<nav>";
+  foreach($items as $item) {
+        $modules=array();
+        if(isset($_SESSION['user_id'])){
+  $s=new autharization();
+  $modules=$s->auth_drow($_SESSION['user_id'], $items['']);
+  //print_r($modules);
+    $html .= "<li><a href='{$item['url']}'>{$item['text']}{$item['value']}</a></li>";
+  }
+  
+  
+
+  
+    
+    else{
+         $html = "<nav>";
   foreach($items as $item) {
     $html .= "<li><a href='{$item['url']}'>{$item['text']}</a></li>";
   }
-  $html .= "</nav>";
+     
+
+    }
+       $html .= "</nav>";
   return $html;
+}
+
 }
 }
 ?>
